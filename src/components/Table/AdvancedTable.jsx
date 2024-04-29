fd802199d91a83fb12b96713cba9fe6315261e1c
Sadia0011 <sadiamoon2019@gmail.com>
2024-04-29 22:03:08 +0600

    added routes forall payroll

--- src/components/Table/AdvancedTable.jsx
+++ src/components/Table/AdvancedTable.jsx
@@ -1,11 +1,112 @@
-import React from 'react';
+
+import React, { useEffect, useState } from 'react';
+import "./AdvancedTable.css"
 
 const AdvancedTable = () => {
+    // pagination
+    const [count,setCount]=useState(0)
+    const [users,setUsers]=useState([])
+    const [realUsers,setRealUsers]=useState([])
+    useEffect(()=>{
+        fetch("https://swiftship-server.vercel.app/itemsCount")
+        .then(res=>res.json())
+        .then(data=>setCount(data))
+        // console.log({count})
+    },[])
+   const total=parseInt(count.count)
+
+    const[itemsPerPage,setItemsPerPage]=useState(5)
+    const[currentPage,setCurrentPage]=useState(0)
+   const numberOfPages=Math.ceil(total/itemsPerPage)
+   console.log( numberOfPages)
+// const pages=[...Array(numberOfPages).keys()]
+// console.log(pages)
+
+const pages=[]
+for(let i=0;i < numberOfPages;i++){
+pages.push(i)
+}
+// console.log(pages)
+
+    // useEffect(() => {
+    //   axiosSecure.get(`/items?page=${currentPage}&size=${itemsPerPage}`)
+    //   .then(res=>{
+    //     setRealUsers(res.data)
+    //     setUsers(res.data)
+    //   })
+    // }, [axiosSecure,currentPage,itemsPerPage]);
+console.log("users",users)
+console.log("real users",realUsers)
+    const handleItemsPerPage=e=>{
+        const val=parseInt(e.target.value);
+        console.log(val)
+        setItemsPerPage(val)
+        setCurrentPage(0)
+    }
+    const handlePrevPage=()=>{
+        if(currentPage>0){
+            setCurrentPage(currentPage-1)
+        }
+    }
+    const handleNextPage=()=>{
+        if(currentPage<pages.length-1){
+            setCurrentPage(currentPage+1)
+        }
+    }
+
     return (
-        <div>
+            <div>
+            <div className="overflow-x-auto">
+  <table className="table  table-xs overflow-x-auto">
+    {/* head */}
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Phone Number</th>
+        <th>Age</th>
+        <th>Company</th>
+      </tr>
+    </thead>
+    <tbody>
+       {users.map((user,index)=>
+      <tr key={user._id}>
+        <th>{index+1}</th>
+        <td>{user?.name}</td>
+        <td>{user?.phone}</td>
+        <td>{user?.age}</td>
+        <td>{user?.company}</td>
+      </tr>)}
+      
+     
+    
+    </tbody>
+  </table>
+</div>
+
+{/* pagination */}
+<div className='pagination'>
+              
+              <button onClick={handlePrevPage}>Prev</button>
+               {pages.map(page=> <button
+               className={currentPage==page ? 'selected' :undefined}
+               onClick={()=>setCurrentPage(page)}
+                key={page}>{page}</button>)}
+                <button onClick={handleNextPage}>Next</button>
+              
+            <select value={itemsPerPage}
+            onChange={handleItemsPerPage}
+            >
+                <option value="5">5</option>
+                <option value="10">10</option>
+                <option value="15">15</option>
+                <option value="50">50</option>
+            </select>
             
-        </div>
+            </div>
+            </div>
     );
 };
 
 export default AdvancedTable;
+
